995609a97a0f7e1673694c77064b4f27b0250ef6
selvaa89 <selva.it.89@gmail.com>
2014-08-27 08:41:27 +0800

    Switch minified/unminifed file based on debug flag.

--- submodules/submodule1/js/app.js
+++ submodules/submodule1/js/app.js
@@ -1,9 +1,11 @@
 "use strict"
 define(['mainApp'], function (App) {
     var submodule1 = angular.module('submodule1', ['ngRoute']);
-    App.register.controller('submodule1.controller.oneController', function ($scope) {
+    console.log('mainApp: ', App);
+    App.register.controller('submodule1.controller.oneController', ['$scope', function ($scope) {
+    	console.log('scope: ', $scope);
         $scope.message = 'This is submodule1';
-    });
+    }]);
     App.refreshRootScope();
     return submodule1;
 });
--- submodules/submodule2/js/app.js
+++ submodules/submodule2/js/app.js
@@ -1,9 +1,9 @@
 "use strict"
 define(['mainApp'], function (App) {
     var submodule2 = angular.module('submodule2', ['ngRoute']);
-    App.register.controller('submodule2.controller.oneController', function ($scope) {
+    App.register.controller('submodule2.controller.oneController', ['$scope', function ($scope) {
         $scope.message = 'This is submodule2';
-    });
+    }]);
     App.refreshRootScope();
     return submodule2;
 });
