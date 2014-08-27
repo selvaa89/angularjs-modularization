962b9f1bd94a76076c9334b9a21a5efaad9e32e4
selvaa89 <selva.it.89@gmail.com>
2014-08-27 09:15:24 +0800

    Removed absolute URLs

--- submodules/submodule1/js/main.js
+++ submodules/submodule1/js/main.js
@@ -2,7 +2,7 @@
 require(['mainApp', 'submodule1/js/app'], function (App, SubModule) {
     App.register.routeProvider
         .when('/submodule/submodule1/one', {
-            templateUrl: '/submodules/submodule1/templates/one.html'
+            templateUrl: 'submodules/submodule1/templates/one.html'
         });
     App.refreshRootScope();
     App.registerModule('submodule1');
--- submodules/submodule2/js/main.js
+++ submodules/submodule2/js/main.js
@@ -2,7 +2,7 @@
 require(['mainApp', 'submodule2/js/app'], function (App, SubModule) {
     App.register.routeProvider
         .when('/submodule/submodule2/one', {
-            templateUrl: '/submodules/submodule2/templates/one.html'
+            templateUrl: 'submodules/submodule2/templates/one.html'
         });
     App.registerModule('submodule2');
     App.refreshRootScope();
